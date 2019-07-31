ccfa3b786f1dc138eb71a20c60bd093c526a2149
AurelijaZuba <auryte00@hotmail.com>
2019-07-31 11:00:18 +0100

    passing test for int 9 and return "Fizz"

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -3,7 +3,7 @@ package com.codurance;
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
         final Integer number = numbers[0];
-        if (number == 3 || number == 6)
+        if (number == 3 || number == 6 || number == 9)
             return "Fizz";
         return number.toString();
     }
