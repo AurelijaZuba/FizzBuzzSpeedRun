053c53479ecc566709feffebc2bdf332373317ea
AurelijaZuba <auryte00@hotmail.com>
2019-07-31 10:52:00 +0100

    Passing test for int 3 and return "Fizz"

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -2,6 +2,8 @@ package com.codurance;
 
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
+        if(numbers[0] == 3)
+            return "Fizz";
        return numbers[0].toString();
     }
 }
