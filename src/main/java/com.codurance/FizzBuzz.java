d73a2a5d8f33961cd947aaa6d45b795900d5d423
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 10:55:14 +0100

    Passing test for int 6 returns "Fizz"

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -4,6 +4,8 @@ public class FizzBuzz {
     public String execute(Integer[] numbers) {
         if (numbers[0] == 3)
             return "Fizz";
+        if (numbers[0] == 6)
+            return "Fizz";
         return numbers[0].toString();
     }
 }
