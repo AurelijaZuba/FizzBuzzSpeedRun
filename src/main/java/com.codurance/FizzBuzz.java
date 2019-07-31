f472d7d17da4f4b1d0c5c35c73ebf40770defdc3
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 10:43:59 +0100

    Refactor to simplify returning string representation of number

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -2,11 +2,6 @@ package com.codurance;
 
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
-        if(numbers[0] == 4)
-            return "4";
-        if(numbers[0] == 2){
-            return "2";
-        }
-        return "1";
+       return numbers[0].toString();
     }
 }
