e6a80d697e676eaa63ae5117a258b2a1952c3d6b
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 10:37:45 +0100

    Add failing test for int 2 returns string 2

--- src/test/java/com.codurance/FizzBuzzShould.java
+++ src/test/java/com.codurance/FizzBuzzShould.java
@@ -15,4 +15,14 @@ public class FizzBuzzShould {
         String expected = "1";
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    void return_string_of_two_when_given_integer_of_two() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Integer[] numbers = {2};
+        String actual = fizzBuzz.execute(numbers);
+
+        String expected = "2";
+        assertThat(actual).isEqualTo(expected);
+    }
 }
