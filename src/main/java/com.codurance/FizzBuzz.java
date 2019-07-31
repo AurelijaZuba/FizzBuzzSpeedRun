61a47f6aedeebc43460693054d85299815f8bedb
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 10:42:54 +0100

    Passing test for int 4 returns 4

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -2,6 +2,8 @@ package com.codurance;
 
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
+        if(numbers[0] == 4)
+            return "4";
         if(numbers[0] == 2){
             return "2";
         }
