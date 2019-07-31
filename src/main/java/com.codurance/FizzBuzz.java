76a50d660ca09b336b27c2fb57422ab8b435207c
AurelijaZuba <auryte00@hotmail.com>
2019-07-31 11:02:12 +0100

    Refactoring - extract isFizz

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -3,8 +3,12 @@ package com.codurance;
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
         final Integer number = numbers[0];
-        if (number == 3 || number == 6 || number == 9)
+        if (isFizz(number))
             return "Fizz";
         return number.toString();
+    }
+
+    private boolean isFizz(Integer number) {
+        return number % 3 == 0;
     }
 }
