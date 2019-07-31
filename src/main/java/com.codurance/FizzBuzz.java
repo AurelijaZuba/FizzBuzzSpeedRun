93f1c4001edfd4c7780e59f208ff0d180ff78d43
AurelijaZuba <auryte00@hotmail.com>
2019-07-31 10:39:41 +0100

    passing test for int 2 return string "2"

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.codurance;
 
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
+        if(numbers[0] == 2){
+            return "2";
+        }
         return "1";
     }
 }
