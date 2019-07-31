5cffa31178981960294d7354c49150d3f1d52ea1
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 10:56:47 +0100

    Refactor extract number variable

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -2,8 +2,9 @@ package com.codurance;
 
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
-        if (numbers[0] == 3 || numbers[0] == 6)
+        final Integer number = numbers[0];
+        if (number == 3 || number == 6)
             return "Fizz";
-        return numbers[0].toString();
+        return number.toString();
     }
 }
