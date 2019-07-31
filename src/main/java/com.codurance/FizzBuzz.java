cace5dc2dcef921e2e33e6ac6a4076c4c6abfd03
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 10:56:15 +0100

    Refactor combine two if statements into one

--- src/main/java/com.codurance/FizzBuzz.java
+++ src/main/java/com.codurance/FizzBuzz.java
@@ -2,9 +2,7 @@ package com.codurance;
 
 public class FizzBuzz {
     public String execute(Integer[] numbers) {
-        if (numbers[0] == 3)
-            return "Fizz";
-        if (numbers[0] == 6)
+        if (numbers[0] == 3 || numbers[0] == 6)
             return "Fizz";
         return numbers[0].toString();
     }
